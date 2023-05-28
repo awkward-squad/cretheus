34f64982eb1a2c77e2aa3320ac4117f72f266c30
Mitchell Rosen <mitchellwrosen@gmail.com>
2023-05-28 11:52:17 -0400

    tweak encode module

--- src/Cretheus/Encode.hs
+++ src/Cretheus/Encode.hs
@@ -1,5 +1,8 @@
 module Cretheus.Encode
-  ( Target,
+  ( -- * Encoding
+    Encoding,
+    bytesEncoding,
+    valueEncoding,
 
     -- * Encoders
     bool,
@@ -13,11 +16,13 @@ where
 import Data.Aeson qualified as Aeson
 import Data.Aeson.Encoding qualified as Aeson
 import Data.Aeson.KeyMap qualified as Aeson.KeyMap
+import Data.ByteString (ByteString)
+import Data.ByteString.Lazy qualified as ByteString.Lazy
 import Data.Text (Text)
 import Data.Vector (Vector)
 import Data.Vector qualified as Vector
 
-class Target a where
+class Encoding a where
   bool_ :: Bool -> a
   int_ :: Int -> a
   text_ :: Text -> a
@@ -25,7 +30,7 @@ class Target a where
   vector_ :: (x -> a) -> Vector x -> a
   object_ :: [(Aeson.Key, a)] -> a
 
-instance Target Aeson.Value where
+instance Encoding Aeson.Value where
   bool_ = Aeson.toJSON
   int_ = Aeson.toJSON
   text_ = Aeson.toJSON
@@ -33,7 +38,7 @@ instance Target Aeson.Value where
   vector_ f = Aeson.Array . Vector.map f
   object_ = Aeson.Object . Aeson.KeyMap.fromList
 
-instance Target Aeson.Encoding where
+instance Encoding Aeson.Encoding where
   bool_ = Aeson.bool
   int_ = Aeson.int
   text_ = Aeson.text
@@ -41,23 +46,36 @@ instance Target Aeson.Encoding where
   vector_ f = Aeson.list f . Vector.toList
   object_ = Aeson.pairs . foldMap (\(k, v) -> Aeson.pair k v)
 
-type Encoder a =
-  forall x. Target x => a -> x
+-- | Interpret an encoding as bytes.
+bytesEncoding :: (forall x. Encoding x => x) -> ByteString
+bytesEncoding encoding =
+  ByteString.Lazy.toStrict (Aeson.encodingToLazyByteString (encoding :: Aeson.Encoding))
 
-bool :: Encoder Bool
+-- | Interpret an encoding as a value.
+valueEncoding :: (forall x. Encoding x => x) -> Aeson.Value
+valueEncoding encoding =
+  encoding
+
+-- | A bool encoder.
+bool :: Encoding x => Bool -> x
 bool = bool_
 
-int :: Encoder Int
+-- | An int encoder.
+int :: Encoding x => Int -> x
 int = int_
 
-text :: Encoder Text
+-- | A text encoder.
+text :: Encoding x => Text -> x
 text = text_
 
-list :: Encoder a -> Encoder [a]
+-- | A list encoder.
+list :: Encoding x => (a -> x) -> [a] -> x
 list e = list_ e
 
-vector :: Encoder a -> Encoder (Vector a)
+-- | A vector encoder.
+vector :: Encoding x => (a -> x) -> Vector a -> x
 vector e = vector_ e
 
-object :: Target a => [(Aeson.Key, a)] -> a
+-- | An object encoder.
+object :: Encoding x => [(Aeson.Key, x)] -> x
 object = object_
